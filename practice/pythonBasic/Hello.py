f043917d5fc07a91785b588ad8d97b18deeb9c72
arif1313 <arifhossen13131@gmail.com>
2024-10-10 14:29:59 +0600

    python practice

---
+++ practice/pythonBasic/Hello.py
@@ -0,0 +1,3 @@
+print("hello world")
+full_name= "arif hossen "
+print(f"hi this is  {full_name}")
---
+++ practice/pythonBasic/ifelse.py
@@ -0,0 +1,7 @@
+age= int(input("enter your age: "))
+if age>=18:
+    print("you are adult")
+elif age>0 and age<18:
+    print("you are child")
+else:
+    print("input right age")
